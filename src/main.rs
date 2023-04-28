7d09cfca1257566b2121b142e96000b6afa5ca03
NWrenger <nils@wrenger.net>
2023-04-28 15:15:32 +0200

    :sparkles: Added update fn

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,15 @@ fn main() -> Result<()> {
         data: None,
     };
 
+    let new_me = User {
+        account: "nils.wrenger".into(),
+        forename: "Nils".into(),
+        surname: "Wrenger".into(),
+        role: "Klasse 10a".into(),
+        criminal: true,
+        data: Some("Hat nen Schuh geklaut!".into()),
+    };
+
     let you = User {
         account: "lars.wrenger".into(),
         forename: "Lars".into(),
@@ -30,8 +39,9 @@ fn main() -> Result<()> {
     println!("All sorted by 'La':{:?}", db::user::search(&db, "La").unwrap());
 
     db::user::delete(&db, &you.account).unwrap();
+    db::user::update(&db, &me.account, &new_me).unwrap();
 
-    println!("Deleted account:lars.wrenger: {:?}", db::user::search(&db, "La").unwrap());
+    println!("Deleted account:lars.wrenger: {:?}", db::user::search(&db, "N").unwrap());
 
     Ok(())
 }
