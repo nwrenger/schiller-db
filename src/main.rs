561f8480629ed0001e6140de2eb8f8a5d7169ce0
NWrenger <nils@wrenger.net>
2023-04-28 22:12:27 +0200

    :sparkles: Done more testing

--- src/main.rs
+++ src/main.rs
@@ -44,26 +44,37 @@ fn main() {
     let presence = Presence {
         date: NaiveDate::from_ymd_opt(2023, 4, 29),
         presenter: me.account.clone(),
+        data: None,
     };
 
-    let lars_presence = Presence {
-        date: NaiveDate::from_ymd_opt(2023, 4, 2),
-        presenter: you.account.clone(),
+    let other_presence = Presence {
+        date: NaiveDate::from_ymd_opt(2023, 4, 30),
+        presenter: me.account.clone(),
+        data: Some("war 5 Min zu Spät".into()),
     };
 
     let new_presence = Presence {
         date: NaiveDate::from_ymd_opt(2023, 4, 2),
         presenter: me.account.clone(),
+        data: Some("Oh doch eher 10 Min".into()),
     };
 
+    let lars_presence = Presence {
+        date: NaiveDate::from_ymd_opt(2023, 4, 2),
+        presenter: you.account.clone(),
+        data: None,
+    };
+
+
     if Database::open(Cow::from(Path::new("./my.db"))).unwrap().1 {
-        db::presence::delete(&db, presence.date).unwrap();
-        db::presence::delete(&db, lars_presence.date).unwrap();
+        db::presence::delete(&db, &presence.presenter, presence.date).unwrap();
+        db::presence::delete(&db, &lars_presence.presenter, lars_presence.date).unwrap();
         db::user::delete(&db, &you.account).unwrap();
         db::user::delete(&db, &me.account).unwrap();
     }
 
     db::presence::add(&db, &presence).unwrap();
+    db::presence::add(&db, &other_presence).unwrap();
     db::presence::add(&db, &lars_presence).unwrap();
     db::user::add(&db, &you).unwrap();
     db::user::add(&db, &me).unwrap();
@@ -77,11 +88,15 @@ fn main() {
         db::presence::search(&db, "wre").unwrap()
     );
 
-    db::presence::update(&db, &me.account, &new_presence).unwrap();
-    println!(
-        "Updated nils.wrenger, sorted by '':{:#?}",
-        db::presence::search(&db, "").unwrap()
-    );
+    db::presence::update(
+        &db,
+        &me.account,
+        NaiveDate::from_ymd_opt(2023, 4, 29),
+        &new_presence,
+    )
+    .unwrap();
+
+    db::presence::delete(&db, &other_presence.presenter, other_presence.date).unwrap();
 
     db::user::update(&db, &me.account, &new_me).unwrap();
 }
