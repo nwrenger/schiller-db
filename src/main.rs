54b35fb5814b6d3b590b1447a5083144ab8d63b5
NWrenger <nils@wrenger.net>
2023-05-16 22:05:35 +0200

    :wrench: Changed Naming

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use utoipa::{
 use utoipa_swagger_ui::SwaggerUi;
 
 use chrono::Local;
-use dotenv::dotenv;
 use std::fs::OpenOptions;
 use std::io::Write;
 
@@ -68,7 +67,7 @@ fn rocket() -> Rocket<Build> {
         }
     };
 
-    dotenv().ok();
+    dotenv::from_filename("keys.env").ok();
 
     #[derive(OpenApi)]
     #[openapi(
