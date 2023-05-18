bfa7dab3f9552057c2466051f2607aa0c0432648
NWrenger <nils@wrenger.net>
2023-05-18 22:39:39 +0200

    :wrench: Changed Logging time to default

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ fn rocket() -> Rocket<Build> {
         LevelFilter::Warn,
         ConfigBuilder::new()
             .set_time_format_rfc3339()
+            .set_time_offset_to_local().unwrap_or(&mut ConfigBuilder::default())
             .set_level_color(Level::Trace, None)
             .set_level_color(Level::Debug, None)
             .set_level_color(Level::Info, None)
