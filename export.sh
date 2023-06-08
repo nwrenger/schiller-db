ef5529bf2a18049abf5cda81e9927316eead7f99
NWrenger <nils@wrenger.net>
2023-06-08 14:04:54 +0200

    :wrench: Finished export.sh

--- export.sh
+++ export.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 cargo build -r
-zip -r exp/sndm_lin.zip target/release/sndm admin.env benutzer.txt static/
+cp target/release/sndm ./
+zip -r exp/sndm_lin.zip sndm admin.env benutzer.txt static/
+rm sndm
 cargo build -r --target x86_64-pc-windows-gnu
-zip -r exp/sndm_win.zip target/x86_64-pc-windows-gnu/release/sndm.exe admin.env benutzer.txt static/
+cp target/x86_64-pc-windows-gnu/release/sndm.exe ./
+zip -r exp/sndm_win.zip sndm.exe admin.env benutzer.txt static/
+rm sndm.exe
