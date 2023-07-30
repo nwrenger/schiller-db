f6661132ba2b86b3a9a16fc9c1080ea015bc5ca8
NWrenger <nils@wrenger.net>
2023-07-30 14:47:13 +0200

    :wrench: Updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 set -e
+npm run build
 cargo build -r
-ssh aws-web rm -r website
+ssh aws rm -r website
 scp -r target/release/schiller-db static/ admin.env benutzer.txt logins.txt schiller-db.db aws-web:website
