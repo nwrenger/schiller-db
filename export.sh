08f1cfe539b22571279a719f9546455f627d7930
NWrenger <nils@wrenger.net>
2023-07-31 00:42:09 +0200

    :wrench: New Build && Fix

--- export.sh
+++ export.sh
@@ -2,7 +2,7 @@
 rm -r ./static/_app/ ./static/bootstrap/ ./static/index.html ./static/login.html
 npm run build
 cross build -r --target x86_64-unknown-linux-gnu
-cp target/release/x86_64-unknown-linux-gnu/schiller-db ./
+cp target/x86_64-unknown-linux-gnu/release/schiller-db ./
 zip -r exp/schiller-db_lin.zip schiller-db admin.env benutzer.txt logins.txt static/
 rm schiller-db
 cargo build -r --target x86_64-pc-windows-gnu
