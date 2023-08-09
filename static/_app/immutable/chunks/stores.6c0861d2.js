c8125d8f73ada5b2067be92bdcbf6a05ad037949
nwrenger <nils@wrenger.net>
2023-08-09 18:40:10 +0200

    :wrench: New Build

---
+++ static/_app/immutable/chunks/stores.6c0861d2.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.7da8f882.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
