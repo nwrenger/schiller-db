570b3617f9f62eecc1a462fcaeb333d83eb88b21
nwrenger <nils@wrenger.net>
2023-08-09 18:40:25 +0200

    :wrench: new Config

--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 rm -r ./static/_app/ ./static/bootstrap/ ./static/index.html ./static/login.html
 npm run build
-cross build -r --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/release/schiller-db ./
+cargo build -r
+cp target/release/schiller-db ./
 zip -r exp/schiller-db_lin.zip schiller-db admin.env benutzer.txt logins.txt static/
 rm schiller-db
 cargo build -r --target x86_64-pc-windows-gnu
