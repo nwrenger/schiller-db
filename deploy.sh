22de227dd2bc1b027fb5ff71053dcf52c8ac2522
NWrenger <nils@wrenger.net>
2023-09-17 12:19:55 +0200

    :wrench:

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 rm -r ./build
 set -e
 bun run build
-cargo build -r
+cross build -r --target x86_64-unknown-linux-gnu
 ssh aws rm -r website
-scp -r target/release/schiller-db build/ admin.env benutzer.txt logins.txt schiller-db.db aws:website
+scp -r target/x86_64-unknown-linux-gnu/release/schiller-db build/ admin.env benutzer.txt logins.txt schiller-db.db aws:website
--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 rm -r ./build
 bun run build
-cargo build -r
-cp target/release/schiller-db ./
+cross build -r --target x86_64-unknown-linux-gnu
+cp target/x86_64-unknown-linux-gnu/release/schiller-db ./
 zip -r exp/schiller-db_lin.zip schiller-db admin.env benutzer.txt logins.txt build/
 rm schiller-db
 cargo build -r --target x86_64-pc-windows-gnu
